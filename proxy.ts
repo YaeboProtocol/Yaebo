ce5b32b9080932bc029199b6b27073ea01a1dc60
Ansh Saxena <anshsaxena4190@gmail.com>
2026-02-16 15:07:34 +0530

    vercel build fix

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@ import { type NextRequest } from 'next/server'
 import { updateSession } from './lib/supabase/middleware'
 
 // Supabase auth/session middleware.
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   return await updateSession(request)
 }
 
